bfa7d3a890b6cb442201dcca7fc54796b0e16a0f
TSITCL\s172025 <hiroyainagaki2018@gmail.com>
2019-10-04 12:02:46 +0900

    Map生成

---
+++ B_TeamProject/Assets/MapScene_Scripts/DebugMouse.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector3 m_clickPosition;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        m_clickPosition = Input.mousePosition;
+        m_clickPosition.z = 10.0f;
+        Vector2 worldMousePos = Camera.main.ScreenToWorldPoint(m_clickPosition);
+
+        transform.position = new Vector3(worldMousePos.x, worldMousePos.y, 0);
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Base0")
+        {
+            Debug.Log("jhfhe");
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("jhfhe");
+                //SceneManager.LoadScene("GameScene");
+            }
+        }
+    }
+
+}
---
+++ B_TeamProject/Assets/MapScene_Scripts/MapGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    //マップ上のオブジェクト個数
+    public const int MAP_BASE_NUM = 7;
+
+    //マップ上のオブジェクト
+    private GameObject[] m_mapObject = new GameObject[MAP_BASE_NUM];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //Texture配列
+        Sprite[] m_sprite = {
+            Resources.Load<Sprite>("Map"),
+            Resources.Load<Sprite>("Forest"),
+            Resources.Load<Sprite>("Forest"),
+            Resources.Load<Sprite>("Forest"),
+            Resources.Load<Sprite>("Forest"),
+            Resources.Load<Sprite>("Forest"),
+            Resources.Load<Sprite>("Forest")};
+
+        //森林テクスチャのロード
+       // Sprite ForestSprite = Resources.Load<Sprite>("Forest");
+
+        for(int i=0;i<MAP_BASE_NUM;i++)
+        {
+            //タイトルのロゴを生成
+            m_mapObject[i] = new GameObject("Base" + i);
+            //スプライトレンダラーをAddする
+            m_mapObject[i].AddComponent<SpriteRenderer>();
+            //リジットを追加
+            m_mapObject[i].AddComponent<Rigidbody2D>();
+            //当たり判定を追加
+            m_mapObject[i].AddComponent<CircleCollider2D>();
+            //重力を０にする
+            m_mapObject[i].GetComponent<Rigidbody2D>().gravityScale = 0;
+            //描画順を変更
+            m_mapObject[i].GetComponent<SpriteRenderer>().sortingOrder = 1;
+
+            //森テクスチャをコンポーネント
+            m_mapObject[i].GetComponent<SpriteRenderer>().sprite = m_sprite[i];
+
+            //m_mapObject[i].transform.position = Instantiate(gameObject);
+        }
+        
+
+       
+
+        //森テクスチャのポジション
+        m_mapObject[0].transform.position = new Vector3(0, 0, 0);
+
+        //マップのスケールを拡大させる
+        m_mapObject[0].transform.localScale = new Vector3(3.5f, 3.5f, 3.5f);
+
+        //マップの回転値の変更
+        m_mapObject[0].transform.transform.rotation = Quaternion.Euler(5, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  
+}
