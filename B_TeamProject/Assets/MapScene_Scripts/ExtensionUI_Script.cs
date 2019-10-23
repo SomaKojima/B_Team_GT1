b6876c793c954eb741ef96a2f988e08b41e2d24d
TSITCL\s172025 <hiroyainagaki2018@gmail.com>
2019-10-23 15:28:51 +0900

    button

---
+++ B_TeamProject/Assets/MapScene_Scripts/ExtensionUI_Script.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtensionUI_Script : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_extension;
+
+    [SerializeField]
+    private GameObject m_sample;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        m_extension.SetActive(false);
+        m_sample.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnClick()
+    {
+        m_extension.SetActive(true);
+        m_sample.SetActive(true);
+    }
+}
