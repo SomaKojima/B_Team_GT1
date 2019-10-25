bfb2f4914221b5f7fd7a7ab4353d4e30124bfb27
TSITCL\s172025 <hiroyainagaki2018@gmail.com>
2019-10-25 09:23:08 +0900

    カメラ切り替えOK

---
+++ B_TeamProject/Assets/MapScene_Scripts/Camera_Change.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Change : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_mainCamera;
+
+    [SerializeField]
+    private GameObject m_subCamera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_mainCamera.SetActive(!m_mainCamera.activeSelf);
+            m_subCamera.SetActive(!m_subCamera.activeSelf);
+        }
+    }
+}
