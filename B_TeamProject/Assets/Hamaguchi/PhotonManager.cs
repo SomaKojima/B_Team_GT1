cb1be4651a97b6b10590acbd8872f5194fda07dc
yoshiki hamaguchi <amakinton1996916@gmail.com>
2019-11-18 16:09:13 +0900

    ロビーシーンの追加

--- B_TeamProject/Assets/Hamaguchi/PhotonManager.cs
+++ B_TeamProject/Assets/Hamaguchi/PhotonManager.cs
@@ -10,6 +10,17 @@ public class PhotonManager : Photon.MonoBehaviour
     private GameObject m_CreateBtnObj;
     [SerializeField]
     private GameObject m_JoinBtnObj;
+    [SerializeField]
+    private GameObject m_LoginBtnObj;
+    [SerializeField]
+    private GameObject m_CreateRoomBtnObj;
+    [SerializeField]
+    private GameObject m_NameInputField;
+    [SerializeField]
+    private GameObject m_RoomNameInputField;
+
+    /// <summary>ルーム名</summary>
+    private string m_RoomName = string.Empty;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +36,7 @@ public class PhotonManager : Photon.MonoBehaviour
 
     void OnJoinedLobby()
     {
-        Debug.Log("OnJoinedLobby()");
+        Debug.Log("ロビーに接続しました");
         //ボタンを押せるようにする
         m_CreateBtnObj.GetComponent<Button>().interactable = true;
         m_JoinBtnObj.GetComponent<Button>().interactable = true;
@@ -40,32 +51,37 @@ public class PhotonManager : Photon.MonoBehaviour
     //ルーム作成
     public void CreateRoom()
     {
-        string userName = "ユーザ1";
-        string userId = "user1";
+        //string userName = "ユーザ1";
+        //string userId = "user1";
         PhotonNetwork.autoCleanUpPlayerObjects = false;
-        //カスタムプロパティ
-        ExitGames.Client.Photon.Hashtable customProp = new ExitGames.Client.Photon.Hashtable();
-        customProp.Add("userName", userName); //ユーザ名
-        customProp.Add("userId", userId); //ユーザID
-        PhotonNetwork.SetPlayerCustomProperties(customProp);
+        ////カスタムプロパティ
+        //ExitGames.Client.Photon.Hashtable customProp = new ExitGames.Client.Photon.Hashtable();
+        //customProp.Add("userName", userName); //ユーザ名
+        //customProp.Add("userId", userId); //ユーザID
+        //PhotonNetwork.SetPlayerCustomProperties(customProp);
 
         RoomOptions roomOptions = new RoomOptions();
-        roomOptions.customRoomProperties = customProp;
+        //roomOptions.customRoomProperties = customProp;
         //ロビーで見えるルーム情報としてカスタムプロパティのuserName,userIdを使いますよという宣言
-        roomOptions.customRoomPropertiesForLobby = new string[] { "userName", "userId" };
+        //roomOptions.customRoomPropertiesForLobby = new string[] { "userName", "userId" };
         roomOptions.maxPlayers = 4; //部屋の最大人数
         roomOptions.isOpen = true; //入室許可する
         roomOptions.isVisible = true; //ロビーから見えるようにする
         //userIdが名前のルームがなければ作って入室、あれば普通に入室する。
-        PhotonNetwork.JoinOrCreateRoom(userId, roomOptions, null);
+        PhotonNetwork.CreateRoom(m_RoomName, roomOptions, null);
     }
 
     //ルーム入室した時に呼ばれるコールバックメソッド
     void OnJoinedRoom()
     {
-        Debug.Log("PhotonManager OnJoinedRoom");
+        Debug.Log("ルームに入りました");
         m_JoinBtnObj.GetComponent<Button>().interactable = false;
         m_CreateBtnObj.GetComponent<Button>().interactable = false;
+    }
+
+    void OnCreateRoom()
+    {
+
     }
 
     //ルーム一覧が取れると
@@ -75,7 +91,7 @@ public class PhotonManager : Photon.MonoBehaviour
         RoomInfo[] rooms = PhotonNetwork.GetRoomList();
         if (rooms.Length == 0)
         {   
-            Debug.Log("ルームが一つもありません");
+            //Debug.Log("ルームが一つもありません");
         }
         else
         {
@@ -93,7 +109,79 @@ public class PhotonManager : Photon.MonoBehaviour
 
     public void JoinRoom()
     {
-        PhotonNetwork.JoinRoom("user1");
+        //PhotonNetwork.JoinRoom("user1");
     }
 
+    //名前決定ボタンを押した際に呼ぶメソッド
+    public void OnLogin()
+    {
+        m_NameInputField.SetActive(false);
+        m_LoginBtnObj.SetActive(false);
+        m_CreateBtnObj.SetActive(true);
+        m_JoinBtnObj.SetActive(true);
+    }
+
+    //部屋作成選択ボタンを押した際に呼ぶメソッド
+    public void CreateScene()
+    {
+        m_CreateBtnObj.SetActive(false);
+        m_JoinBtnObj.SetActive(false);
+        m_RoomNameInputField.SetActive(true);
+        m_CreateRoomBtnObj.SetActive(true);
+    }
+
+    //名前の入力欄が変更された時に呼ばれるコールバックメソッド
+    public void InputLogger()
+    {
+        string inputValue = m_NameInputField.GetComponent<InputField>().text;
+
+        PhotonNetwork.playerName = inputValue;
+
+        Debug.Log("名前:" + PhotonNetwork.playerName);
+
+        m_LoginBtnObj.GetComponent<Button>().interactable = true;
+
+        if (inputValue=="")
+        {
+            m_LoginBtnObj.GetComponent<Button>().interactable = false;
+        }
+    }
+
+    //部屋名の入力欄が変更された時に呼ばれるコールバックメソッド
+    public void InputRoomLogger()
+    {
+        string inputValue = m_NameInputField.GetComponent<InputField>().text;
+
+        m_RoomName = inputValue;
+
+        Debug.Log("部屋:" + m_RoomName);
+
+        m_CreateRoomBtnObj.GetComponent<Button>().interactable = true;
+
+        if (inputValue == "")
+        {
+            m_CreateRoomBtnObj.GetComponent<Button>().interactable = false;
+        }
+    }
+
+    //サーバーに到達できず接続できない時に呼ばれるコールバックメソッド
+    void OnFailedToConnectToPhoton(DisconnectCause cause)
+    {
+        Debug.Log("接続に失敗しました:" + cause.ToString());
+        //SceneManager.LoadScene("TitleScene");
+    }
+
+    //何かのせいで(接続が確立した後)接続失敗した時に呼ばれるコールバックメソッド
+    void OnConnectionFail(DisconnectCause cause)
+    {
+        Debug.Log("サーバーとの接続後に何らかの原因で切断されました:" + cause.ToString());
+        //SceneManager.LoadScene("TitleScene");
+    }
+
+    //同時接続可能数の制限に到達した時に呼ばれるコールバックメソッド
+    void OnPhotonMaxCccuReached()
+    {
+        Debug.Log("サーバーに接続しているクライアント数が上限に達しています");
+        //SceneManager.LoadScene("TitleScene");
+    }
 }
