5c5eaf5cf80734d85e44bd2387cb76fc232f426d
yoshiki hamaguchi <amakinton1996916@gmail.com>
2019-11-20 16:18:32 +0900

    送受信、途中経過

--- B_TeamProject/Assets/Hamaguchi/PVManager.cs
+++ B_TeamProject/Assets/Hamaguchi/PVManager.cs
@@ -9,10 +9,22 @@ public class PVManager : Photon.MonoBehaviour
     public int hensu1 = 0;
     public float hensu2 = 0f;
 
+    private PlayerInfo[] playerInfos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 4; i++) 
+        {
+            playerInfos[i].Area1Point = 0;
+            playerInfos[i].Area2Point = 0;
+            playerInfos[i].Area3Point = 0;
+            playerInfos[i].Area4Point = 0;
+            playerInfos[i].Area5Point = 0;
+            playerInfos[i].Area6Point = 0;
+            playerInfos[i].TradeFlag = false;
+            playerInfos[i].PlayerID = i + 1;
+        }
     }
 
     // Update is called once per frame
@@ -28,12 +40,36 @@ public class PVManager : Photon.MonoBehaviour
             //データの送信
             stream.SendNext(hensu1);
             stream.SendNext(hensu2);
+
+            for (int i = 0; i < 4; i++)
+            {
+                stream.SendNext(playerInfos[i].Area1Point);
+                stream.SendNext(playerInfos[i].Area2Point);
+                stream.SendNext(playerInfos[i].Area3Point);
+                stream.SendNext(playerInfos[i].Area4Point);
+                stream.SendNext(playerInfos[i].Area5Point);
+                stream.SendNext(playerInfos[i].Area6Point);
+                stream.SendNext(playerInfos[i].TradeFlag);
+                stream.SendNext(playerInfos[i].PlayerID);
+            }
         }
         else
         {
             //データの受信
             this.hensu1 = (int)stream.ReceiveNext();
             this.hensu2 = (int)stream.ReceiveNext();
+
+            for (int i = 0; i < 4; i++)
+            {
+                this.playerInfos[i].Area1Point = (int)stream.ReceiveNext();
+                this.playerInfos[i].Area2Point = (int)stream.ReceiveNext();
+                this.playerInfos[i].Area3Point = (int)stream.ReceiveNext();
+                this.playerInfos[i].Area4Point = (int)stream.ReceiveNext();
+                this.playerInfos[i].Area5Point = (int)stream.ReceiveNext();
+                this.playerInfos[i].Area6Point = (int)stream.ReceiveNext();
+                this.playerInfos[i].TradeFlag = (bool)stream.ReceiveNext();
+                this.playerInfos[i].PlayerID = (int)stream.ReceiveNext();
+            }
         }
     }
 }
--- B_TeamProject/Assets/Hamaguchi/PlayerInfo.cs
+++ B_TeamProject/Assets/Hamaguchi/PlayerInfo.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerInfo : MonoBehaviour
 {
+    public int Area1Point;
+    public int Area2Point;
+    public int Area3Point;
+    public int Area4Point;
+    public int Area5Point;
+    public int Area6Point;
+    public bool TradeFlag;
+    public int PlayerID;
 
     // Start is called before the first frame update
     void Start()
